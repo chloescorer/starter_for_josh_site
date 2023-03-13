32412836a5943de2f2ffd8a110bc5f9ac6c471e0
chloescorer <chloe02@outlook.com>
2023-03-13 14:52:25 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,15 @@
+<footer class="container-fluid text-center">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <a class="navbar-brand" href="#"><span class="josh">JOSH</span> <span class="orange">WHITKIN</span></a>
+      </div> <!-- Column -->
+      <div class="col-md-6">
+        <p>Terms and Conditions</p>
+      </div> <!-- Column -->
+    </div> <!-- Row -->
+  </div> <!-- Container-->
+</footer> <!-- Fluid Container -->
+<?php wp_footer(); ?>
+</body>
+</html>
